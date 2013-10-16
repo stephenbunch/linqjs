4d139b9557c23651752048a24bd55bb772d6343b
Stephen Bunch <bunchclone@gmail.com>
2013-10-15 22:31:40 -0700

    Added take, skip and step.

--- dist/linq.js
+++ dist/linq.js
@@ -202,10 +202,10 @@ linq.extend(
     select: function( selector )
     {
         selector = linq.lambda( selector );
-        var self = this, e = self.enumerator();
+        var self = this;
         return linq.enumerable( function()
         {
-            var current = null;
+            var current = null, e = self.enumerator();
             this.current = function() {
                 return current;
             };
@@ -226,10 +226,10 @@ linq.extend(
     where: function( filter )
     {
         filter = linq.lambda( filter );
-        var self = this, e = self.enumerator();
+        var self = this;
         return linq.enumerable( function()
         {
-            var current = null;
+            var current = null, e = self.enumerator();
             this.current = function() {
                 return current;
             };
@@ -244,6 +244,80 @@ linq.extend(
                     }
                 }
                 return false;
+            };
+        });
+    },
+
+    /**
+     * @description Gets the first 'x' number of items.
+     * @param {number} number
+     * @returns {Enumerable}
+     */
+    take: function( number )
+    {
+        var self = this;
+        return linq.enumerable( function()
+        {
+            var e = self.enumerator(), count = 0;
+            this.current = function() {
+                return e.current();
+            };
+            this.next = function() {
+                return count++ < number && e.next();
+            };
+        });
+    },
+
+    /**
+     * @description Excludes the first 'x' number of items.
+     * @param {number} number
+     * @returns {Enumerable}
+     */
+    skip: function( number )
+    {
+        var self = this;
+        return linq.enumerable( function()
+        {
+            var e = self.enumerator(), count = 0;
+            this.current = function() {
+                return e.current();
+            };
+            this.next = function()
+            {
+                while ( count < number && e.next() )
+                    count++;
+                return e.next();
+            };
+        });
+    },
+
+    /**
+     * @description
+     * Skips every other 'x' item. A number of 1 includes every item. A number of 2 skips
+     * every other item.
+     * @param {number} number
+     * @returns {Enumerable}
+     */
+    step: function( number )
+    {
+        var self = this;
+        return linq.enumerable( function()
+        {
+            var e = self.enumerator(), started = false;
+            this.current = function() {
+                return e.current();
+            };
+            this.next = function()
+            {
+                if ( !started )
+                {
+                    started = true;
+                    return e.next();
+                }
+                var i = 1;
+                while ( i < number && e.next() )
+                    i++;
+                return e.next();
             };
         });
     }
--- spec/generators.spec.js
+++ spec/generators.spec.js
@@ -19,3 +19,27 @@ describe( "enumerable.where", function()
         expect( e.array() ).toEqual([ 2,4 ]);
     });
 });
+
+describe( "enumerable.take", function()
+{
+    it( "should take the first 'x' number of items", function()
+    {
+        expect( from([ 1,2,3 ]).take( 2 ).array() ).toEqual([ 1,2 ]);
+    });
+});
+
+describe( "enumerable.skip", function()
+{
+    it( "should skip the first 'x' number of items", function()
+    {
+        expect( from([ 1,2,3 ]).skip( 2 ).array() ).toEqual([ 3 ]);
+    });
+});
+
+describe( "enumerable.step", function()
+{
+    it( "should skip every other 'x' item", function()
+    {
+        expect( from([ 1,2,3 ]).step( 2 ).array() ).toEqual([ 1,3 ]);
+    });
+});
